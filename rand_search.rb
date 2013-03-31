0271bc7134a1a7b612b71ae126d4ec0234cd0add
Nan Ma <nma@yammer-inc.com>
2013-03-30 23:22:47 -0700

    adjust number of neighbors

--- rand_search.rb
+++ rand_search.rb
@@ -2,9 +2,11 @@ require_relative 'lib/variance'
 require_relative 'lib/all_perms'
 require_relative 'lib/perm_check'
 
-num_players = 5
-size = 30
-num_iter = 10
+num_players = 4
+size = 12
+num_iter = 100
+num_neighbors = 400
+neighbor_distance = 2
 
 perms = all_perms(num_players)
 num_perms = perms.length
@@ -15,7 +17,7 @@ state_arr = [0]
   state_arr << rand(num_perms)
 end
 # alternatively you may set the initial value
-state_arr = [0, 114, 55, 63, 93, 103, 60, 85, 58, 98, 118, 27, 36, 23, 0, 83, 49, 100, 65, 102, 62, 56, 26, 18, 74, 107, 114, 66, 63, 13]
+# state_arr = [0, 114, 55, 63, 93, 103, 60, 85, 58, 98, 118, 27, 36, 23, 0, 83, 49, 100, 65, 102, 62, 56, 26, 18, 74, 107, 114, 66, 63, 13]
 
 # convert state_arr to string
 state_str = state_arr.map{|index| perms[index]}.join
@@ -27,38 +29,45 @@ end
 
 best = [state_arr, state_str, state_score]
 
-num_iter.times do 
+num_iter.times do |iter_index|
+  num_neighbors = (50 * (iter_index ** 0.5) + 1).to_i
+
   p [state_str, state_score]
 
   neighbors_arr = []
   neighbors_str = []
   neighbors_score = []
 
-  (size - 1).times do |perm_position|
-    (num_perms - 1).times do |perturb_offset|
-      neighbor_state_arr = Array.new(state_arr)
-      neighbor_state_arr[perm_position + 1] += perturb_offset
-      neighbor_state_arr[perm_position + 1] %= num_perms
-      neighbor_state_str = neighbor_state_arr.map{|index| perms[index]}.join
-      neighbor_state_score = score(neighbor_state_str, num_players)
+  num_neighbors.times do 
+    neighbor_state_arr = Array.new(state_arr)
 
-      if neighbor_state_score < 0.0000001
-        puts 'A neighbor is optimal'
-        p [neighbor_state_arr, neighbor_state_str, neighbor_state_score]
-        output_file = File.open('output.txt', 'w')
-        output_file.write(neighbor_state_str)
-        return
-      end
-
-      if neighbor_state_score < best[2]
-        best = [neighbor_state_arr, neighbor_state_str, neighbor_state_score]
-      end
-
-      neighbors_arr << neighbor_state_arr
-      neighbors_str << neighbor_state_str
-      neighbors_score << neighbor_state_score
-
+    neighbor_distance.times do 
+      rand_pos = rand(size - 1) + 1
+      neighbor_state_arr[rand_pos] += rand(num_perms - 1) + 1
+      neighbor_state_arr[rand_pos] %= num_perms
     end
+
+    neighbor_state_str = neighbor_state_arr.map{|index| perms[index]}.join
+    neighbor_state_score = score(neighbor_state_str, num_players)
+
+    if neighbor_state_score < 0.0000001
+      puts 'A neighbor is optimal'
+      p [neighbor_state_arr, neighbor_state_str, neighbor_state_score]
+      output_file = File.open('permutation_fair.txt', 'w')
+      output_file.write(neighbor_state_str)
+      return
+    end
+
+    if neighbor_state_score < best[2]
+      best = [neighbor_state_arr, neighbor_state_str, neighbor_state_score]
+      output_file = File.open('best_so_far.txt', 'w')
+      output_file.write(best)
+      output_file.flush
+    end
+
+    neighbors_arr << neighbor_state_arr
+    neighbors_str << neighbor_state_str
+    neighbors_score << neighbor_state_score
   end
 
   if neighbors_score.min < state_score
@@ -93,13 +102,16 @@ num_iter.times do
       end
     end
     selected ||= 0
-
+    # puts "before"
+    # p state_arr
+    # puts "after"
     state_arr = neighbors_arr[selected]
     state_score = neighbors_score[selected]
+    
   end
 end
 
 puts "Cannot find permutation fair dice after #{num_iter} round of search. The best so far is:"
 p best
-output_file = File.open('output.txt', 'w')
+output_file = File.open('best_so_far.txt', 'w')
 output_file.write(best)
